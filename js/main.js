20a3ddad94f84be7f5cd79033c14c3df1b3a646e
ksewka <54828553+ksewka@users.noreply.github.com>
2020-02-04 23:05:54 +1200

    баг в функции подбора случайного значения

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
-var USER_NUMBER = [1, 2, 3, 4, 5, 6, 7, 8];
 var PRICE = [2000, 2500, 3000, 3500];
 var CHECK_IN = ['12:00', '13:00', '14:00'];
 var CHECK_OUT = ['12:00', '13:00', '14:00'];
@@ -12,15 +11,9 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
 var apartmentsAmount = 8;
-var PIN_WIDTH = 40;
-var PIN_HEIGHT = 44;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
 var mapWidth = document.querySelector('.map').clientWidth;
-
-// Функция для подбора случайного значения в массиве
-var getRandom = function (array) {
-  var random = Math.floor(Math.random() * array.length);
-  return array[random];
-};
 
 // Функция подбора случайного числа в заданном промежутке
 var getRandomNumber = function (min, max) {
@@ -33,46 +26,43 @@ var getRandomLength = function (array) {
   return array;
 };
 
-// Функция возвращает отсортированный случайным образм массив
-var shuffle = function (array) {
-  array.sort(() => Math.random() - 0.5);
-  return array;
-};
-
-shuffle(USER_NUMBER);// Перетасовали массив
-
-// Генерируем массив из 8 объектов
-var apartments = [];
-for (var i = 0; i < apartmentsAmount; i++) {
-  var userNumber = '0' + USER_NUMBER[i];
-  var location_x = getRandomNumber(130, 630);
-  var location_y = getRandomNumber(0, mapWidth);
+// Функция для создания одного объекта
+var getApartment = function(index) {
+  var locationX = getRandomNumber(0, mapWidth);
+  var locationY = getRandomNumber(130, 630);
   var apartment = {
-    'author': {
-      'avatar': 'img/avatars/user' + userNumber + '.png'
+    author: {
+      avatar: 'img/avatars/user0' + (index + 1) + '.png'
     },
 
-    'offer': {
-      'title': getRandom(TYPE),
-      'address': location_x + ', '+ location_y,
-      'price': getRandom(PRICE),
-      'type': getRandom(TYPE),
-      'rooms': getRandomNumber(1, 5),
-      'guests': getRandomNumber(1, 8),
-      'checkin': getRandom(CHECK_IN),
-      'checkout': getRandom(CHECK_OUT),
-      'features': getRandomLength(FEATURES),
-      'description': 'some_description',
-      'photos': getRandomLength(PHOTOS)
+    offer: {
+      title: TYPE[getRandomNumber(0, TYPE.length)],
+      address: locationX + ', ' + locationY,
+      price: PRICE[getRandomNumber(0, PRICE.length)],
+      type: TYPE[getRandomNumber(0, TYPE.length)],
+      rooms: getRandomNumber(1, 5),
+      guests: getRandomNumber(1, 8),
+      checkin: CHECK_IN[getRandomNumber(0, CHECK_IN.length)],
+      checkout: CHECK_OUT[getRandomNumber(0, CHECK_OUT.length)],
+      features: getRandomLength(FEATURES),
+      description: 'someDescription',
+      photos: getRandomLength(PHOTOS)
     },
 
-    'location': {
-      'x': location_x,
-      'y': location_y
+    location: {
+      x: locationX,
+      y: locationY
     }
   };
-  apartments.push(apartment);
+  return apartment;
+};
+
+// Создаем массив из 8 объектов
+var apartments = [];
+for (var i = 0; i < apartmentsAmount; i++) {
+  apartments.push(getApartment(i));
 }
+console.log(apartments);
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
@@ -82,10 +72,10 @@ var pinTemplate = document.querySelector('#pin')
   .querySelector('.map__pin');
 
 // Функция клонирует элемент и заполняет данными из массива объектов
-var renderPin = function(pin) {
+var renderPin = function (pin) {
   var pinElement = pinTemplate.cloneNode(true);
   pinElement.style.left = pin.location.x + PIN_WIDTH / 2 + 'px';
-  pinElement.style.top = pin.location.y + PIN_HEIGHT + 'px';
+  pinElement.style.top = pin.location.y - PIN_HEIGHT + 'px';
   pinElement.querySelector('img').src = pin.author.avatar;
   pinElement.querySelector('img').alt = pin.offer.title;
   return pinElement;
